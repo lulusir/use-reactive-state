1b9b6713c045da43b96dbdd18ac36a93db41603c
lujs <327240969@qq.com>
2020-11-03 11:08:15 +0800

    feat: docs

---
+++ docs/demo.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { createReactiveState, useReactiveState } from 'use-reactive-state';
+
+class Vm {
+  name = 'use-react-state';
+
+  setName(name: string) {
+    this.name = name;
+  }
+}
+
+const vm = createReactiveState(new Vm());
+
+const ComA = () => {
+  const state = useReactiveState(vm);
+  return (
+    <div>
+      <p>name1:{state.name}</p>
+    </div>
+  );
+};
+
+const ComB = () => {
+  const state = useReactiveState(vm);
+  return (
+    <div>
+      <p>name2:{state.name}</p>
+    </div>
+  );
+};
+
+const Index = () => {
+  const state = useReactiveState(vm);
+
+  return (
+    <div>
+      <ComA />
+      <ComB />
+      <p>name:{state.name}</p>
+      <button
+        type="button"
+        onClick={() => {
+          const n = Math.random().toString();
+          vm.setName(n);
+        }}
+      >
+        set name
+      </button>
+    </div>
+  );
+};
+
+export default Index;
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,8 @@
 ## Hello use-reactive-state!
+
+### usage
+
+<code src="./demo.tsx" transform/>
+```typescript
+import { createReactiveState, useReactiveState } from 'use-reactive-state';
+```
